ad5e49b461c3f03b9568f54db81dc61b51d16e19
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-27 07:38:26 +0900

    ルートヘルパー名修正_testが通る

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get messages_index_url
+    get messages_url
     assert_response :success
   end
 
   test "should get create" do
-    get messages_create_url
+    get messages_url
     assert_response :success
   end
 end
