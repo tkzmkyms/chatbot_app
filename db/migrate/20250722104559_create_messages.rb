4de008b8908acc03f09e86fc3943d818177a804a
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-28 06:57:05 +0900

    レンダーエラー2

--- db/migrate/20250722104559_create_messages.rb
+++ db/migrate/20250722104559_create_messages.rb
@@ -1,4 +1,4 @@
-class CreateMessages < ActiveRecord::Migration[7.1]
+class CreateMessages < ActiveRecord::Migration[7.1] # 7.1で安定
   def change
     create_table :messages do |t|
       t.text :content
